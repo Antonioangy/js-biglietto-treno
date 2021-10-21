3fef9c46984b84974e9e47aa7386df1878d546da
Antonioangy <dumitruangy26@gmail.com>
2021-10-21 15:40:00 +0200

    aggiunta km e eta' passeggero

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
-
+let numerokm = prompt('inserisci quanti chilometri vuoi percorrere')
 // l’età del passeggero
-
+let eta = prompt('inserisci la tua eta o quella del passeggero')
 // // calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // va applicato uno sconto del 20% per i minorenni
