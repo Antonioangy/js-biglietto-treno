4f4de304d9909b05635275decf80ddbf8810651a
Antonioangy <dumitruangy26@gmail.com>
2021-10-21 15:45:38 +0200

    aggiunta prezzo totale numerokm*prezzoalkm

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@
 let numerokm = prompt('inserisci quanti chilometri vuoi percorrere')
 // l’età del passeggero
 let eta = prompt('inserisci la tua eta o quella del passeggero')
-// // calcolare il prezzo totale del viaggio, secondo queste regole:
+
+// REGOLE 
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+const prezzoalkm = 0.21;
+
+// prezzo a km x km da percorrere 
+let prezzototale = numerokm * prezzoalkm; 
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
