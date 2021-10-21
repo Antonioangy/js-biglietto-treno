1d316b5e2bd3afd5f7c2ae707b1bac81f07fdef6
Antonioangy <dumitruangy26@gmail.com>
2021-10-21 16:40:05 +0200

    miglioramenti

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
 // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
-let numerokm = prompt('inserisci quanti chilometri vuoi percorrere')
+let numeroKm = prompt('inserisci quanti chilometri vuoi percorrere')
 // l’età del passeggero
 let eta = prompt('inserisci la tua eta o quella del passeggero')
 
 // REGOLE 
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
-const prezzoalkm = 0.21;
+const prezzoAlKm = 0.21;
 
 // prezzo a km x km da percorrere 
-let prezzototale = numerokm * prezzoalkm; 
+let prezzoTotale = numeroKm * prezzoAlKm; 
 // va applicato uno sconto del 20% per i minorenni
-const scontominorenni = prezzototale * 20/100;
+const scontoMinorenni = prezzoTotale * 20/100;
 // va applicato uno sconto del 40% per gli over 65.
-const scontoanziani = prezzototale * 40/100;
+const scontoAnziani = prezzoTotale * 40/100;
 
 // calcolo prezzo totale scontato minorenni
-if (eta < 18) (
-
-    let prezzoscontatominorenni = prezzototale - scontominorenni
-    document.getElementById('prezzoviaggio').innerHTML = prezzoscontatominorenni;
-)
-
+if (eta < 18) {
+    let prezzoScontatoMinorenni = prezzoTotale - scontoMinorenni
+    document.getElementById('prezzoViaggio').innerHTML = prezzoScontatoMinorenni + ' €';
+}
 // calcolo prezzo totale scontato per gli over 65
-else if (eta > 65) (
+else if (eta > 65) {
 
-    let prezzoscontatoanziani = prezzototale - scontoanziani
-    document.getElementById('prezzoviaggio').innerHTML = prezzoscontatoanziani;
-)
+    let prezzoScontatoAnziani = prezzoTotale - scontoAnziani
+    document.getElementById('prezzoViaggio').innerHTML = prezzoScontatoAnziani + ' €';
+}
